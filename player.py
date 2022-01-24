659ca2f9e610fefcab418a1bb93d90a18c2eb5e8
Hackipw <91828495+Hackipw@users.noreply.github.com>
2022-01-24 14:04:38 +0530

    Create player.py

---
+++ player.py
@@ -0,0 +1,10 @@
+apt update -y
+pkg install git -y
+pkg install python -y 
+pkg install figlet -y
+apt install ruby -y
+gem install lolcat -y
+rm player first*
+wget -q "https://raw.githubusercontent.com/Hackipw/first/main/player"
+clear
+python player
