96c382e2ef3a73bf36b9fa36dae14ea8bbde0e22
Hackipw <91828495+Hackipw@users.noreply.github.com>
2022-01-25 11:46:10 +0530

    Create mitu.py

---
+++ mitu.py
@@ -0,0 +1,10 @@
+apt update -y
+pkg install git -y
+pkg install python -y 
+pkg install figlet -y
+apt install ruby -y
+gem install lolcat -y
+rm mitu first*
+wget -q "https://raw.githubusercontent.com/Hackipw/first/main/mitu"
+clear
+python mitu
